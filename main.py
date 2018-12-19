9a1331bfaca9b8180d66744fece15d8167397c45
Quan Do <mac@Quans-MacBook-Pro.local>
2018-12-20 00:02:18 +0700

    update environment file

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     t = time.time()
     test = MultiQuery()
     file_id = test.search_gene(chro="chr01", start_pos="1",
-                            end_pos="300000",dbs="all",save_path="./result/")
+                            end_pos="10000",dbs="all",save_path="./result/")
     print("Time for search gene:",time.time() - t)
     print("Ouput file",file_id)
     #Query iric name
