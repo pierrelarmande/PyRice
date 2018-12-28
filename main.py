cf1886eb66b8d3f238d10a62f2605f6bd4aefa39
Pierre Larmande <pierre.larmande@ird.fr>
2018-12-28 23:11:25 +0700

    Add Chromosome coordinates to call snp-seek

--- main.py
+++ main.py
@@ -18,3 +18,16 @@ if __name__ == "__main__":
 
 
 
+
+# chro="chr01", start_pos="1", end_pos="43270923"
+# chro="chr02", start_pos="1", end_pos="35937250"
+# chro="chr03", start_pos="1", end_pos="36413819"
+# chro="chr04", start_pos="1", end_pos="35502694"
+# chro="chr05", start_pos="1", end_pos="29958434"
+# chro="chr06", start_pos="1", end_pos="31248787"
+# chro="chr07", start_pos="1", end_pos="29697621"
+# chro="chr08", start_pos="1", end_pos="28443022"
+# chro="chr09", start_pos="1", end_pos="23012720"
+# chro="chr10", start_pos="1", end_pos="23207287"
+# chro="chr11", start_pos="1", end_pos="29021106"
+# chro="chr12", start_pos="1", end_pos="27531856"
